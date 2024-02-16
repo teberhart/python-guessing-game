49996035628e22f1b61f91f1ad84671cc516fd33
teberhart <thibaut.eberhart@gmail.com>
2024-02-16 16:24:34 +0100

    init - created the base of the game

---
+++ main.py
@@ -0,0 +1,36 @@
+QUESTION_1 = "What is the biggest country in the world ? "
+ANSWER_1 = "Russia"
+Q1 = [QUESTION_1, ANSWER_1]
+
+QUESTION_2 = "What is the most spoken language in the world ? "
+ANSWER_2 = "Chinese"
+Q2 = [QUESTION_2, ANSWER_2]
+
+QUESTION_3 = "Paris is known as the city of ... ? "
+ANSWER_3 = "Love"
+Q3 = [QUESTION_3, ANSWER_3]
+
+QUESTION_4 = "Bacon comes from what animal ? "
+ANSWER_4 = "Pig"
+Q4 = [QUESTION_4, ANSWER_4]
+
+QUESTIONS = [Q1, Q2, Q3, Q4]
+
+def main():
+    correct = 0
+    name = input("What's your name, player ? ")
+
+    print(f"Hello {name} ! Welcome to our quiz game !")
+    print(f"Today, for you, we have {len(QUESTIONS)} questions for you !")
+
+    for question in QUESTIONS:
+        answer = input(question[0])
+        if answer.lower() == question[1].lower():
+            print("Correct !")
+            correct += 1
+        else:
+            print("Wrong !")
+    
+    print(f"You had {correct} right answers, out of {len(QUESTIONS)} possible ones.")
+
+main()
